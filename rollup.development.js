2eb0dbb919328597f75001854f92fac6648104a4
Martin Rossil <martinrossil@gmail.com>
2021-03-25 23:51:26 +0100

    Created dev rollup and tsconfig.

---
+++ rollup.development.js
@@ -0,0 +1,24 @@
+import typescript from 'rollup-plugin-typescript2';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import resolve from '@rollup/plugin-node-resolve';
+
+export default [{
+        input: './src/EntaDev.ts',
+        plugins: [
+            typescript({ tsconfig: 'tsconfig.development.json' }),
+            resolve(),
+            serve({
+                contentBase: 'development',
+                open: true,
+                historyApiFallback: true
+            }),
+            livereload()
+        ],
+        output: {
+            file: './development/esnext.js',
+            format: 'esm',
+            sourcemap: 'inline'
+        }
+    }
+]
