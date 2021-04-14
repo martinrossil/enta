13940fc444d2a620f2092a96c1976327dc81b44e
Martin Rossil <martinrossil@gmail.com>
2021-04-14 14:38:46 +0200

    Modified interface.

--- src/interfaces/svg/ISurfaceElement.ts
+++ src/interfaces/svg/ISurfaceElement.ts
@@ -1,11 +1,11 @@
 import BlurFilter from '../../filters/BlurFilter';
 import ShadowFilter from '../../filters/ShadowFilter';
 import { CornerType } from '../../types/CornerType';
-import ISizeElement from '../core/ISizeElement';
 import IColor from '../vo/IColor';
 import ILinearGradient from '../vo/ILinearGradient';
+import IPathElement from './IPathElement';
 
-export default interface ISurfaceElement extends ISizeElement {
+export default interface ISurfaceElement extends IPathElement {
     cornerSize: number;
     cornerType: CornerType;
     strokeColor: IColor | ILinearGradient | null;
