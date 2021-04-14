b27f3eb66e8702647b5bbf240acece24b2e167be
Martin Rossil <martinrossil@gmail.com>
2021-04-14 14:35:11 +0200

    Added removeElement & removeElements methods.

--- src/interfaces/core/IDisplayContainer.ts
+++ src/interfaces/core/IDisplayContainer.ts
@@ -5,6 +5,8 @@ import ILayoutElement from './ILayoutElement';
 export default interface IDisplayContainer extends IDisplayElement {
     addElement(element: ILayoutElement): void;
     addElements(elements: Array<ILayoutElement>): void;
+    removeElement(element: ILayoutElement): void;
+    removeElements(): void;
     padding: number;
     paddingLeft: number;
     paddingTop: number;
