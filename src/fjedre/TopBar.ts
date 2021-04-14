f2a7b4b21f47624d41a68e3cef8c755e322ce18b
Martin Rossil <martinrossil@gmail.com>
2021-04-14 20:59:26 +0200

    Updated TopBar.

--- src/fjedre/TopBar.ts
+++ src/fjedre/TopBar.ts
@@ -14,7 +14,7 @@ export default class TopBar extends DisplayContainer {
         this.name = 'TopBar';
         this.layoutData = new AnchorLayoutData(NaN, NaN, NaN, NaN, 100);
         this.paddingX = 16;
-        this.height = 80;
+        this.height = 64;
         this.backgroundColor = new Color(0, 0, 100); // white
         this.addFilter(new BoxShadowFilter(0, 1, 1, NaN, new Color(0, 0, 0, 0.05)));
         this.layout = new HorizontalLayout(0, 'fill', 'middle');
