51b9d77aed70c3afc817b3bdcdd646c495d7b269
Martin Rossil <martinrossil@gmail.com>
2021-04-15 10:13:20 +0200

    Updated index.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { default as HorizontalLayoutSpacer } from './components/HorizontalLayoutSpacer';
 export { Icons } from './components/Icons';
 export { default as Square } from './components/Square';
 export { default as SquareContainer } from './components/SquareContainer';
@@ -8,6 +9,7 @@ export { default as DisplayContainer } from './core/DisplayContainer';
 export { default as DisplayElement } from './core/DisplayElement';
 export { default as LayoutElement } from './core/LayoutElement';
 export { default as PositionElement } from './core/PositionElement';
+export { default as ScrollContainer } from './core/ScrollContainer';
 export { default as SizeElement } from './core/SizeElement';
 
 export { Strings } from './enums/Strings';
@@ -19,6 +21,9 @@ export { default as BoxShadowFilter } from './filters/BoxShadowFilter';
 export { default as ShadowFilter } from './filters/ShadowFilter';
 
 export { default as BottomBar } from './fjedre/BottomBar';
+export { default as LanguageSelector } from './fjedre/LanguageSelector';
+export { default as Logo } from './fjedre/Logo';
+export { default as Search } from './fjedre/Search';
 export { default as TopBar } from './fjedre/TopBar';
 
 export { default as Machine } from './fsm/Machine';
@@ -29,6 +34,7 @@ export { default as IDisplayContainer } from './interfaces/core/IDisplayContaine
 export { default as IDisplayElement } from './interfaces/core/IDisplayElement';
 export { default as ILayoutElement } from './interfaces/core/ILayoutElement';
 export { default as IPositionElement } from './interfaces/core/IPositionElement';
+export { default as IScrollContainer } from './interfaces/core/IScrollContainer';
 export { default as ISizeElement } from './interfaces/core/ISizeElement';
 
 export { default as IEventDispatcher } from './interfaces/event/IEventDispatcher';
@@ -43,6 +49,8 @@ export { default as IState } from './interfaces/fsm/IState';
 
 export { default as IAnchorLayout } from './interfaces/layout/IAnchorLayout';
 export { default as IAnchorLayoutData } from './interfaces/layout/IAnchorLayoutData';
+export { default as IHorizontalLayout } from './interfaces/layout/IHorizontalLayout';
+export { default as IHorizontalLayoutData } from './interfaces/layout/IHorizontalLayoutData';
 export { default as ILayout } from './interfaces/layout/ILayout';
 export { default as ILayoutData } from './interfaces/layout/ILayoutData';
 export { default as IVerticalLayout } from './interfaces/layout/IVerticalLayout';
@@ -52,12 +60,20 @@ export { default as IPathElement } from './interfaces/svg/IPathElement';
 export { default as ISurfaceElement } from './interfaces/svg/ISurfaceElement';
 export { default as ISvgElement } from './interfaces/svg/ISvgElement';
 
+export { default as IBaseText } from './interfaces/text/IBaseText';
+export { default as ILabelElement } from './interfaces/text/ILabelElement';
+export { default as ITextElement } from './interfaces/text/ITextElement';
+export { default as ITextRenderer } from './interfaces/text/ITextRenderer';
+
 export { default as IColor } from './interfaces/vo/IColor';
 export { default as ILinearGradient } from './interfaces/vo/ILinearGradient';
 export { default as IRectangle } from './interfaces/vo/IRectangle';
+export { default as ITypeFace } from './interfaces/vo/ITypeFace';
 
 export { default as AnchorLayout } from './layout/AnchorLayout';
 export { default as AnchorLayoutData } from './layout/AnchorLayoutData';
+export { default as HorizontalLayout } from './layout/HorizontalLayout';
+export { default as HorizontalLayoutData } from './layout/HorizontalLayoutData';
 export { default as VerticalLayout } from './layout/VerticalLayout';
 export { default as VerticalLayoutData } from './layout/VerticalLayoutData';
 
@@ -65,11 +81,20 @@ export { default as PathElement } from './svg/PathElement';
 export { default as SurfaceElement } from './svg/SurfaceElement';
 export { default as SvgElement } from './svg/SvgElement';
 
+export { default as BaseText } from './text/BaseText';
+export { default as LabelElement } from './text/LabelElement';
+export { default as TextRenderer } from './text/TextRenderer';
+
+export { ClipType } from './types/ClipType';
+export { CornerType } from './types/CornerType';
+export { FontWeight } from './types/FontWeight';
 export { HorizontalAlign } from './types/HorizontalAlign';
+export { TextAlign } from './types/TextAlign';
 export { VerticalAlign } from './types/VerticalAlign';
 
 export { default as Color } from './vo/Color';
 export { default as LinearGradient } from './vo/LinearGradient';
 export { default as Rectangle } from './vo/Rectangle';
+export { default as TypeFace } from './vo/TypeFace';
 
 export { default as EntaDev } from './EntaDev';
