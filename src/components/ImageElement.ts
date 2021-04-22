00408fd92a5e3cc9fce5964285aa05afe8e4b141
Martin Rossil <martinrossil@gmail.com>
2021-04-22 09:35:46 +0200

    Created ImageElement.

---
+++ src/components/ImageElement.ts
@@ -0,0 +1,42 @@
+import DisplayElement from '../core/DisplayElement';
+import { Strings } from '../enums/Strings';
+import IImageElement from '../interfaces/components/IImageElement';
+
+export default class ImageElement extends DisplayElement implements IImageElement {
+    public constructor() {
+        super();
+        this.name = 'Image';
+        this.appendChild(this.img);
+    }
+
+    protected validate(): void {
+        super.validate();
+        this.img.width = this.measuredWidth;
+        this.img.height = this.measuredHeight;
+    }
+
+    private _img!: HTMLImageElement;
+
+    private get img(): HTMLImageElement {
+        if (!this._img) {
+            this._img = document.createElement('img');
+            this._img.style.objectFit = Strings.COVER;
+        }
+        return this._img;
+    }
+
+    private _source = '';
+
+    public set source(value: string) {
+        if (this._source === value) {
+            return;
+        }
+        this._source = value;
+        this.img.src = value;
+    }
+
+    public get source(): string {
+        return this._source;
+    }
+}
+customElements.define('image-element', ImageElement);
