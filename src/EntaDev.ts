b2aac7872a39ce1161240e877c8a016e9c9425ae
Martin Rossil <martinrossil@gmail.com>
2021-04-22 09:50:41 +0200

    Removed test code.

--- src/EntaDev.ts
+++ src/EntaDev.ts
@@ -1,21 +1,9 @@
 import ApplicationElement from './core/ApplicationElement';
-import LeftNavigationList from './fjedre/LeftNavigationList';
-import Theme from './fjedre/Theme';
-import TopBar from './fjedre/TopBar';
-import UniqueSellingPoints from './fjedre/UniqueSellingPoints';
-import AnchorLayout from './layout/AnchorLayout';
 
 export default class EntaDev extends ApplicationElement {
     public constructor() {
         super();
         this.name = 'EntaDev';
-        this.backgroundColor = Theme.blueGray100;
-        this.layout = new AnchorLayout();
-        window.addEventListener('load', () => {
-            this.addElement(new TopBar());
-            this.addElement(new LeftNavigationList());
-            this.addElement(new UniqueSellingPoints());
-        });
     }
 }
 customElements.define('enta-dev', EntaDev);
