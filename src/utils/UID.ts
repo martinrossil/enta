7646c88bd18d9a70c77c408cc854daa80a40be63
Martin Rossil <martinrossil@gmail.com>
2021-07-03 21:24:07 +0200

    Refactored, crypto not implemented in IE11.

--- src/utils/UID.ts
+++ src/utils/UID.ts
@@ -1,11 +1,5 @@
 export default class UID {
-    public static get(length = 21): string {
-        let unique = '';
-        const random = crypto.getRandomValues(new Uint8Array(length));
-        for (;length--;) {
-            const number = 63 & random[length];
-            unique += number < 36 ? number.toString(36) : number < 62 ? (number - 26).toString(36).toUpperCase() : number < 63 ? '_' : '-';
-        }
-        return unique;
+    public static get(): string {
+        return Math.random() + '';
     }
 }
