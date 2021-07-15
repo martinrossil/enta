c19965be313f71e713c34d4d2601d772f43e49f0
Martin Rossil <martinrossil@gmail.com>
2021-07-15 11:53:03 +0200

    Added measure* for optimization.

--- src/interfaces/core/ISizeElement.ts
+++ src/interfaces/core/ISizeElement.ts
@@ -2,8 +2,16 @@ import IPositionElement from './IPositionElement';
 
 export default interface ISizeElement extends IPositionElement {
     size(width: number, height: number): void;
+    externalSize(width: number, height: number): void;
     width: number;
     height: number;
     percentWidth: number;
     percentHeight: number;
+    measureInternalSize: boolean;
+    measureInternalWidth: boolean;
+    measureInternalHeight: boolean;
+    externalWidth: number;
+    externalHeight: number;
+    readonly measuredWidth: number;
+    readonly measuredHeight: number;
 }
