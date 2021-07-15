c129ceee387d2078afb821d7388a06b17346e218
Martin Rossil <martinrossil@gmail.com>
2021-07-15 11:54:34 +0200

    Removed properties that's in super interface.

--- src/interfaces/svg/ISurfaceElement.ts
+++ src/interfaces/svg/ISurfaceElement.ts
@@ -1,15 +1,7 @@
-import BlurFilter from '../../filters/BlurFilter';
-import ShadowFilter from '../../filters/ShadowFilter';
 import { CornerType } from '../../types/CornerType';
-import IColor from '../vo/IColor';
-import ILinearGradient from '../vo/ILinearGradient';
 import IPathElement from './IPathElement';
 
 export default interface ISurfaceElement extends IPathElement {
     cornerSize: number;
     cornerType: CornerType;
-    strokeColor: IColor | ILinearGradient | null;
-    fillColor: IColor | ILinearGradient | null;
-    strokeWidth: number;
-    addFilter(filter: BlurFilter | ShadowFilter): void;
 }
