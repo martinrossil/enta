8a8634330a00c737b5ba6ab99eadd68216d0bdcf
Martin Rossil <martinrossil@gmail.com>
2021-07-21 13:42:35 +0200

    Fixed broken test.

--- tests/IApplicationElement.spec.ts
+++ tests/IApplicationElement.spec.ts
@@ -1,7 +1,6 @@
 import { assert } from 'chai';
 import { describe, it } from 'mocha';
 import { ApplicationElement } from '../src';
-customElements.define('a-e', ApplicationElement);
 const applicationElement: ApplicationElement = new ApplicationElement();
 
 describe('IApplicationElement interface', () => {
